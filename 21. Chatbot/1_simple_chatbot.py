dba3969e59f3b7032f695f9f50b3ac10099565fc
Anekant Jain <anekant.employee@gmail.com>
2026-01-11 23:13:37 +0530

    Renamed

--- 21. Chatbot/1_simple_chatbot.py
+++ 21. Chatbot/1_simple_chatbot.py
@@ -1,44 +1,56 @@
-from dotenv import load_dotenv
-from typing import TypedDict, Annotated
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage, AIMessage
 from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, START, END
-from langgraph.checkpoint.memory import MemorySaver
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import HumanMessage, BaseMessage
+from typing import TypedDict, Annotated, List
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
 
 class ChatState(TypedDict):
-    messages: Annotated[list[BaseMessage], add_messages]
+    messages: Annotated[List[BaseMessage], add_messages]
+
+
+prompt_template = ChatPromptTemplate(
+    [
+        (SystemMessage, "You're a helpful chatbot"),
+        (HumanMessage, "Write a response to this message: {message}"),
+    ]
+)
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+output_parser = StrOutputParser()
+chain = prompt_template | llm | output_parser
+
+
+def chat_node(state: ChatState):
+    messages = state["messages"]
+
+    response = chain.invoke(messages)
+
+    return {"messages": [AIMessage(response)]}
+
 
 graph = StateGraph(ChatState)
 
-def chat(state: ChatState) -> ChatState:
-    messages = state['messages']
-    response = model.invoke(messages)
-    
-    return {'messages': [response]}
+graph.add_node("chat", chat_node)
 
-graph.add_node('chat', chat)
+graph.add_edge(START, "chat")
+graph.add_edge("chat", END)
 
-graph.add_edge(START, 'chat')
-graph.add_edge('chat', END)
+chatbot = graph.compile()
 
-checkpointer = MemorySaver()
-chatbot = graph.compile(checkpointer=checkpointer)
-
-thread_id = '1'
 while True:
-    input_text = input("User: ")
-    config = {'configurable': {'thread_id': thread_id}}
+    text = input("User: ")
+    text = text.strip()
     
-    if input_text.strip().lower() == 'exit':
-        print(chatbot.get_state(config=config))
+    if text.lower() == 'exit':
+        print("Bye")
         break
     
-    result = chatbot.invoke({'messages': [HumanMessage(content=input_text)]}, config=config)
+    results = chatbot.invoke({"messages": [HumanMessage(text)]})
+    print(f"AI:", results["messages"][-1].content)
     
-    print("AI: ", result['messages'][-1].content)
     
